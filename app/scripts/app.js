acd661266f95dd60e5ce20e9f89f8385fecc860a
Anna Thomas <annathom@uk.ibm.com>
2016-09-13 11:01:25 +0100

    Generate yeoman app from angular-generator

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spotlightNewsApp
+ * @description
+ * # spotlightNewsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spotlightNewsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotlightNewsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spotlightNewsApp
+ */
+angular.module('spotlightNewsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
